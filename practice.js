cae48b906c8be8003116fab38009a58052b3dd7d
syedamir5560 <syedamir5560@gmail.com>
2023-11-16 13:27:39 +0530

    ss

--- practice.js
+++ practice.js
@@ -1,30 +1,40 @@
 let btn = document.querySelector("button")
 let cont = document.querySelector(".container")
 
-let xhr = new XMLHttpRequest()
-xhr.open("GET", "https://fakestoreapi.com/users", true)
+// let xhr = new XMLHttpRequest()
+// xhr.open("GET", "https://fakestoreapi.com/users", true)
 
-function fun() {
+// function fun() {
 
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let x = JSON.parse(this.responseText)
-            console.log(x)
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             let x = JSON.parse(this.responseText)
+//             console.log(x)
 
 
-            x.forEach((element, index) => {
-                let newb = `<div class='card'>
-            <h2>Name : ${element.name.firstname}</h2>
-            <h3>Phone : ${element.phone}</h3>
-            <h4>Password : ${element.password}</h4>
-            <address>Email : ${element.email}</address>
-             </div>`
-            
-             cont.insertAdjacentHTML('beforeend', newb)
-             btn.disabled="true"
-            
-            });
+//             x.forEach((element, index) => {
+//                 let newb = `<div class='card'>
+//             <h2>Name : ${element.name.firstname}</h2>
+//             <h3>Phone : ${element.phone}</h3>
+//             <h4>Password : ${element.password}</h4>
+//             <address>Email : ${element.email}</address>
+//              </div>`
+
+//              cont.insertAdjacentHTML('beforeend', newb)
+//              btn.disabled="true"
+
+//             });
+//         }
+//     }
+//     xhr.send()
+// }
+
+
+fetch("https://fakestoreapi.com/users")
+
+    .then(Response => {
+        if (Response.status == 200) {
+
+            console.log(Response)
         }
-    }
-    xhr.send()
-}
+    })
