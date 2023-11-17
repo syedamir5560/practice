39bcac76ee014f7cf3ac77b2c64859247d9d8ce9
syedamir5560 <syedamir5560@gmail.com>
2023-11-17 23:58:06 +0530

    dd

--- practice.js
+++ practice.js
@@ -1,32 +1,3 @@
-let btn = document.querySelector("button")
-let cont = document.querySelector(".container")
 
-// let xhr = new XMLHttpRequest()
-// xhr.open("GET", "https://fakestoreapi.com/users", true)
-
-// function fun() {
-
-//     xhr.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             let x = JSON.parse(this.responseText)
-//             console.log(x)
-
-
-//             x.forEach((element, index) => {
-//                 let newb = `<div class='card'>
-//             <h2>Name : ${element.name.firstname}</h2>
-//             <h3>Phone : ${element.phone}</h3>
-//             <h4>Password : ${element.password}</h4>
-//             <address>Email : ${element.email}</address>
-//              </div>`
-
-//              cont.insertAdjacentHTML('beforeend', newb)
-//              btn.disabled="true"
-
-//             });
-//         }
-//     }
-//     xhr.send()
-// }
 
     
