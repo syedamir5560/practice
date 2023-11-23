79c6a4db43ef5f4a55846ad1925e4c7c66063c45
syedamir5560 <syedamir5560@gmail.com>
2023-11-23 12:32:41 +0530

    6

--- practice.js
+++ practice.js
@@ -1,15 +1,3 @@
 
 
-document.getElementById('submit').addEventListener("click",(e)=>{
-    e.preventDefault()
-    checkData()
-})
-
-let username = document.getElementById('username')
-
-function checkData(){
-    let usernameValue = username.value.trim()
-
-
-}
-    
+ 
