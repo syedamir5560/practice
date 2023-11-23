c2ea67ccb4b81a965790690ddc4653fdbcac31e9
syedamir5560 <syedamir5560@gmail.com>
2023-11-23 13:17:21 +0530

    cd

--- practice.js
+++ practice.js
@@ -1,3 +1,12 @@
+let form = document.querySelector('#create-account-form')
+let userInput = document.querySelector('#username')
+let emailInput = document.querySelector('#email')
+let passwordInput = document.querySelector('#password')
+let confirmpasswordInput = document.querySelector('#confirlm-password')
 
+form.addEventListener('submit',(e)=>{
+e.preventDefault()
+validateForm()
+})
 
- 
+cd
