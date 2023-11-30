85523c843c120ec031c8356c8abad7d20c60283e
syedamir5560 <syedamir5560@gmail.com>
2023-11-30 15:25:05 +0530

    debounce

--- practice.js
+++ practice.js
@@ -1,68 +1,74 @@
+
+
 let form = document.querySelector('#create-account-form');
 let usernameInput = document.querySelector('#username')
 let emailInput = document.querySelector('#email')
 let passwordInput = document.querySelector('#password')
 let confirmpasswordInput = document.querySelector('#confirm-password')
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    validateForm()
-    
+
+    getData()
 })
 
-function validateForm() {
-    let valid = true;
-    // Username
+
+function debounce(call, d) {
+    let timer;
+    return function (...args) {
+        if (timer) clearTimeout(timer);
+        setTimeout(() => {
+            call();
+        }, d)
+    }
+}
+
+const mainF = debounce(getData, 1500);
+
+function getData() {
+
     if (usernameInput.value.trim() == '') {
         setError(usernameInput, 'Username Cannot Be Blank', 'p1')
-
     } else if (usernameInput.value.trim().length < 5 || usernameInput.value.trim().length > 15) {
         setError(usernameInput, 'Username Cannot Be Less than 5 or more than 15 letters', 'p1')
-    
     } else {
         setSuccess(usernameInput)
-     
     }
 
-    // Email
+    // email
+
     if (emailInput.value.trim() == '') {
         setError(emailInput, 'email Cannot Be Blank', 'p2')
-      
     } else if (isValidEmail(emailInput.value) == false) {
         setError(emailInput, 'Invalid Email', 'p2')
-
     } else {
         setSuccess(emailInput)
- 
-
     }
 
     //Password
+
     if (passwordInput.value.trim() == '') {
         setError(passwordInput, 'Password Cannot Be Blank', 'p3')
-        
     } else if (passwordInput.value.trim().length < 8 || passwordInput.value.trim().length > 20) {
         setError(passwordInput, 'Password Cannot Be Less than 8 or more than 20 characters', 'p3')
-    
     } else if (isvalidpass(passwordInput.value) == false) {
         setError(passwordInput, 'Password should include lowercase, uppercase, special characters', 'p3')
-      
     }
     else {
         setSuccess(passwordInput)
-  
     }
 
     //conferm-password
     if (confirmpasswordInput.value.trim() == '') {
         setError(confirmpasswordInput, 'Password Cannot Be Blank', 'p4')
- 
+
     } else if (confirmpasswordInput.value.localeCompare(passwordInput.value, { sensitivity: "base" })) {
         setError(confirmpasswordInput, 'Password does not match', 'p4')
     }
     else {
         setSuccess(confirmpasswordInput)
-      
+
     }
 }
 
@@ -86,7 +92,7 @@ function setSuccess(element) {
 
 function isValidEmail(para) {
     const reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    // console.log(reg.test(para));
+    console.log(reg.test(para));
     return reg.test(para)
 }
 
@@ -94,3 +100,6 @@ function isvalidpass(para) {
     let reg = /^[a-zA-Z0-9!@#$%^&*]{8,20}$/
     return reg.test(para)
 }
+
+
+
