0e6b772caa0a3f157fd5d965cc06fd06650f5875
syedamir5560 <syedamir5560@gmail.com>
2023-12-11 13:18:00 +0530

    f

--- practice.js
+++ practice.js
@@ -1,23 +1,12 @@
-let intNum = 1234
+let num=1234
 
-let myArr = String(intNum).split("").map((intNum)=>{
-    return Number(intNum)
-})
-console.log(myArr)
+let numstr=num.toString();
+let sum=1 ;
+for(let digit of numstr){
+    
+    sum *= parseInt(digit);
+}
+console.log("Sum Of Digit is "+sum);
 
-// Declare a variable and store an
-// integer value
-// let num = 235345
 
-// // Here we typecasting the num
-// // Splitting the num, so that
-// // we got an array of strings
-// // Then use map function to
-// // convert the array of strings
-// // into array of numbers
 
-// let myArr = String(num).split("").map((num) => {
-// 	return Number(num)
-// })
-
-// console.log(myArr)
