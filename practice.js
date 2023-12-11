7d9d33ef129f74a8df1e27226d44f3e62d698bda
syedamir5560 <syedamir5560@gmail.com>
2023-12-11 12:45:51 +0530

    f

--- practice.js
+++ practice.js
@@ -1,8 +1,23 @@
+let intNum = 1234
 
+let myArr = String(intNum).split("").map((intNum)=>{
+    return Number(intNum)
+})
+console.log(myArr)
 
+// Declare a variable and store an
+// integer value
+// let num = 235345
 
+// // Here we typecasting the num
+// // Splitting the num, so that
+// // we got an array of strings
+// // Then use map function to
+// // convert the array of strings
+// // into array of numbers
 
+// let myArr = String(num).split("").map((num) => {
+// 	return Number(num)
+// })
 
-
-
-
+// console.log(myArr)
