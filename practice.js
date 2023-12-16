f11ec3e868654473c7486d628d98d37154bd96e1
syedamir5560 <syedamir5560@gmail.com>
2023-12-16 19:28:12 +0530

    new

--- practice.js
+++ practice.js
@@ -1,9 +1,1 @@
-function squaredDigits(num) {
-    let digits = num.toString().split('');
-    let squared = digits.map(digit => digit = digit*digit);
-    return parseInt(squared.join(''));
-   }
 
-
-
-   console.log(squaredDigits(123)); // Output: 149
